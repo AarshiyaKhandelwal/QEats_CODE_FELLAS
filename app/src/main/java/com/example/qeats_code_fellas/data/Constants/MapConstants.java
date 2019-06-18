707e6da43103a6dfa0df9bef4b31ae36c48d471d
crio-user <aarshiya.khandelwal@gmail.com>
2019-06-18 22:17:40 +0530

    Front end

---
+++ app/src/main/java/com/example/qeats_code_fellas/data/Constants/MapConstants.java
@@ -0,0 +1,15 @@
+package com.example.qeats_code_fellas.data.Constants;
+
+public class MapConstants {
+
+    public static final String APPREFERENCES_NAME = "SwigymapDemoPrefs";
+
+    public static final String USER_ADD_ID = "user_add_id";
+
+    public static final String ADDRESS = "address";
+    public static final String NEWADDRESS = "newaddress";
+    public static final String LATITUDE = "latitude";
+    public static final String LONGITUDE = "longitude";
+
+    public static final String ZERO_RESULTS = "ZERO_RESULTS";
+}
